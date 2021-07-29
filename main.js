1e84155afdfe54502d2a68783d8ef66419489a5c
kukuruza <iamhab@gmail.com>
2021-07-30 00:51:47 +1000

    Добавление задачи

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
 const form = document.querySelector('#addForm');
+const itemsList = document.querySelector('#items');
 const textInput = document.querySelector('#newItemText');
 const deleteBtn = document.querySelector('button');
 
 
+// function addTodo(event) {
+//   event.preventDefault();
+//   let inputTodo = textInput.value;
+//   const itemTodo = `<li class="list-group-item">${inputTodo} <button data-action="delete" type="button" class="btn btn-light btn-sm float-right">Удалить</button></li>`;
+
+
+//   if(!inputTodo) {
+//     alert('Ошибка. Пустое поле')
+//   } else (
+//     alert('Задача добавлено')
+//   )
+
+
+//   items.insertAdjacentHTML('afterbegin', itemTodo);
+// }
+
 function addTodo(event) {
   event.preventDefault();
-  let newTodo = textInput.value;
-  const items = document.querySelector('#items');
-  const item = `<li class="list-group-item">${newTodo} <button data-action="delete" type="button" class="btn btn-light btn-sm float-right">Удалить</button></li>`;
+
+  let inputTodo = textInput.value;
+  let itemTodo = document.createElement('li');
+  let btnDelete = document.createElement('button');
+
+  itemTodo.appendChild(document.createTextNode(inputTodo));
+  itemTodo.className = 'list-group-item';
+
+  btnDelete.appendChild(document.createTextNode('Удалить'));
+  btnDelete.className = 'btn btn-light btn-sm float-right';
+  btnDelete.setAttribute('data-action', 'delete')
+
+  itemTodo.appendChild(btnDelete);
 
 
-  if(!newTodo) {
-    alert('Ошибка. Пустое поле')
-  } else (
-    alert('Задача добавлено')
-  )
+  if(!inputTodo) {
+    console.log('Ошибка ! Вы не ввели задачу');
+    return false
+  } else {
+    console.log('Вы добавили задачу');
+  }
 
-
-  items.insertAdjacentHTML('afterbegin', item);
+  itemsList.insertAdjacentElement('afterbegin', itemTodo);
 }
-
 
 
 
