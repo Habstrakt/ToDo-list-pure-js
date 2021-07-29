17c5d340b30a7b1834588c5e7677d57c3617bf34
kukuruza <iamhab@gmail.com>
2021-07-30 00:10:45 +1000

    Добавление задачи через строку

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 const form = document.querySelector('#addForm');
 const textInput = document.querySelector('#newItemText');
-const deleteBtn = document.querySelectorAll('button');
+const deleteBtn = document.querySelector('button');
 
-console.log(deleteBtn);
 
 function addTodo(event) {
   event.preventDefault();
-  console.log(textInput.value);
+  let newTodo = textInput.value;
+  const items = document.querySelector('#items');
+  const item = `<li class="list-group-item">${newTodo} <button data-action="delete" type="button" class="btn btn-light btn-sm float-right">Удалить</button></li>`;
+
+
+  if(!newTodo) {
+    alert('Ошибка. Пустое поле')
+  } else (
+    alert('Задача добавлено')
+  )
+
+
+  items.insertAdjacentHTML('afterbegin', item);
 }
 
 
 
+
+
 form.addEventListener('submit', addTodo);
+
